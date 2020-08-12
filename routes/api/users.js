b864aa2be034fab179b1c8292909d658ac63ab4a
sk <skbro10@gmail.com>
2020-08-12 21:43:44 +0545

    Authentication for User in Backend - login - register - getatoken - protect routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config')
 
 const {
   check,
   validationResult
 } = require('express-validator/check');
 
-const User = require('../../models/User')
+const User = require('../../models/User');
 
 
 // @route POST api/users
@@ -40,7 +42,7 @@ router.post('/', [
         email
       });
       if (user) {
-        res.status(400).json({
+        return res.status(400).json({
           errors: [{
             msg: 'User already exists'
           }]
@@ -64,18 +66,27 @@ router.post('/', [
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      // Return jsonwebtoken
 
-      res.send('User Registered');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+      jwt.sign(payload, config.get('jwtSecret'), {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({
+            token
+          });
+        });
+
 
     } catch (err) {
       console.error(err.message);
       res.status(500).send(' Server Error')
-
     }
-
-
-
   });
 
 module.exports = router;
