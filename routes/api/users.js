fca33ffcfc31b5bdb2d3087fc7a9dd0ebb9dff4f
sk <skbro10@gmail.com>
2020-08-12 20:05:02 +0545

    Registration - User Registration Completed - Added Validation - Added Gravatar on package.json for user Image - Added bycryptjs for hashing the password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs')
 
 const {
   check,
   validationResult
 } = require('express-validator/check');
+
+const User = require('../../models/User')
 
 
 // @route POST api/users
@@ -18,14 +22,60 @@ router.post('/', [
       min: 6
     })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({
         errors: errors.array()
       });
     }
-    res.send('User route')
+    const {
+      name,
+      email,
+      password
+    } = req.body;
+
+    try {
+      let user = await User.findOne({
+        email
+      });
+      if (user) {
+        res.status(400).json({
+          errors: [{
+            msg: 'User already exists'
+          }]
+        });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      })
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      // Return jsonwebtoken
+
+      res.send('User Registered');
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send(' Server Error')
+
+    }
+
+
+
   });
 
 module.exports = router;
